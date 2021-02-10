e496cd068b36b46d8d8adc837ad71ae7f9b35aa0
mani-barathi <manibarathi.s@gmail.com>
2021-02-10 19:14:55 +0530

    photo upload selection

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
-import React, { } from 'react'
+import React, { useRef, useState } from 'react'
 import "../css/Nav.css"
 import { useDispatch, useSelector } from "react-redux"
-import { IconButton, Avatar, Button, Tooltip } from "@material-ui/core"
+import { IconButton, Avatar, Button, Tooltip, Typography, Snackbar } from "@material-ui/core"
 import SearchIcon from '@material-ui/icons/Search'
 import PublishIcon from '@material-ui/icons/Publish'
 
@@ -9,6 +9,8 @@ import { setUser } from "../actions"
 import { auth } from "../firebase"
 
 function Nav() {
+    const fileRef = useRef()
+    const [uploadMessage, setUploadMessage] = useState('')
     const dispatch = useDispatch()
     const user = useSelector(state => state)
 
@@ -18,17 +20,31 @@ function Nav() {
             .catch(err => console.log(err.message))
     }
 
+    const getUploadImages = () => {
+        fileRef.current.click()
+    }
+
+    const handleUploadImage = () => {
+        const photos = fileRef.current.files
+        if (photos.length === 0) return
+
+        console.log(photos)
+        setUploadMessage(`Uploading ${photos.length} Photo`)
+    }
+
     return (
         <div className="nav">
             <div className="nav__logo">
-                <span className="nav__logoText" style={{ color: "#4285F4" }}>G</span>
-                <span className="nav__logoText" style={{ color: "#DB4437" }}>o</span>
-                <span className="nav__logoText" style={{ color: "#F4B400" }}>o</span>
-                <span className="nav__logoText" style={{ color: "#4285F4" }}>g</span>
-                <span className="nav__logoText" style={{ color: "#0F9D58" }}>l</span>
-                <span className="nav__logoText" style={{ color: "#DB4437" }}>e</span>
+                <Typography variant="h5" >
+                    <span style={{ color: "#4285F4" }}>G</span>
+                    <span style={{ color: "#DB4437" }}>o</span>
+                    <span style={{ color: "#F4B400" }}>o</span>
+                    <span style={{ color: "#4285F4" }}>g</span>
+                    <span style={{ color: "#0F9D58" }}>l</span>
+                    <span style={{ color: "#DB4437" }}>e</span>
                 &nbsp;
                 <span className="nav__logoText2" >Photos</span>
+                </Typography>
             </div>
 
             <div className="nav_search">
@@ -37,10 +53,9 @@ function Nav() {
             </div>
 
             <div className="nav__right">
-                <Button
-                    startIcon={<PublishIcon />}
-                    size="small"
-                    className="nav__rightUploadBtn"
+                <Button startIcon={<PublishIcon />}
+                    size="small" className="nav__rightUploadBtn"
+                    onClick={getUploadImages}
                 >
                     Upload
                 </Button>
@@ -51,6 +66,17 @@ function Nav() {
                     </IconButton>
                 </Tooltip>
             </div>
+
+            <input type="file" onChange={handleUploadImage} ref={fileRef}
+                multiple accept="image/*" style={{ display: "none" }} />
+
+            {
+                <Snackbar
+                    open={Boolean(uploadMessage)}
+                    anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                    message={uploadMessage} />
+            }
+
 
         </div >
     )
